ebfaf28d24d0cc39d5c2770aedd68b6448c6869a
ManInDark <janknd13@gmail.com>
2021-09-29 23:20:21 +0200

    Changed Zeichen to Letters in CompileAndCopy.cmd to reflect the changed name of the file.

--- CompileAndCopy.cmd
+++ CompileAndCopy.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "AutoHotkey Script Zeichen.ahk" /out "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotkey Script Zeichen.exe"
+"C:\Program Files\AutoHotkey\Compiler\Ahk2Exe.exe" /in "AutoHotkey Script Letters.ahk" /out "C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\AutoHotkey Script Letters.exe"
